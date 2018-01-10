076f2604ee2e4c7b06dba6135149d6ae37489fab
Alan Wilder <wilderalan@gmail.com>
2018-01-10 10:25:30 -0500

    finish resume section

--- _site/assets/resume.js
+++ _site/assets/resume.js
@@ -8,9 +8,13 @@ revealClickers.forEach(function(clicker){
 function expandResumeSection(e) {
   var popup = document.querySelector(`#${this.dataset.reveal}`);
   popup.classList.toggle('active')
-
+  document.querySelectorAll(`[data-reveal="${this.dataset.reveal}"]`)
+  .forEach(function(element){
+    element.classList.toggle('active');
+  })
   $('html, body').animate({
-    scrollTop: Math.min(popup.offsetParent.offsetTop, document.body.offsetHeight - window.innerHeight + 300)
+    scrollTop: Math.min(popup.offsetParent.offsetTop, document.body.offsetHeight - window.innerHeight + popup.offsetParent.dataset.offset)
   }, 1000)
 
 }
+// Note: offset for each di hardcoded into html via popup.offsetParent.dataset.offset
